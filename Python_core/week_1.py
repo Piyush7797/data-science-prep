0b56461264d0359055d2a6cabc79cead9a1534d7
Piyush vishvakrma <piyushvishvakrma7797email@gmail.com>
2026-04-10 14:12:24 +0530

    Day 1: Python basics (week 1)

---
+++ Python_core/week_1.py
@@ -0,0 +1,14 @@
+#Week 1: Basics – Variables, Data Types, Input/Output, Loops, Conditionals
+
+
+#Write a Python program to swap two variables
+
+a =  5
+b = 10
+print("Before swapping: a =", a, "b =", b)
+# Swapping
+print("After swapping: a =", b, "b =", a)
+
+a ,b = b,a 
+print("a = ", a, "b = ", b)
+
