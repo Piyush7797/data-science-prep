0e6f587f5d51fcb1ed21c1cc2b87776266f48aee
Piyush vishvakrma <piyushvishvakrma7797email@gmail.com>
2026-04-10 14:29:01 +0530

    Day 1: Python basics (week 1)

--- Python_core/week_1.py
+++ Python_core/week_1.py
@@ -12,3 +12,23 @@ print("After swapping: a =", b, "b =", a)
 a ,b = b,a 
 print("a = ", a, "b = ", b)
 
+#Q2. Take user input and display it back to the user
+
+num = int(input("Enter the number "))
+name =  input("enter your name")
+
+print("the number are ",num)
+print("the name is ",name)
+
+#Q3. Write a program to check if a number is even or odd
+num =  int(input("enter the number "))
+
+if num % 2 ==0 :
+    print(num, " number is even")
+else :
+    print(num, "number is odd")
+
+#Create a program that prints the multiplication table of a given number.
+num = int(input("enterthe number"))
+for i in range(1 , 11):
+    print(num,"x",i,"=",num*i)
